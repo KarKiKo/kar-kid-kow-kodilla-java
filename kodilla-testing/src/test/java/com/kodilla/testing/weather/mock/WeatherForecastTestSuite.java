a0e575f95dd3b6b95f1e7062bd97cbd2f961bde9
KarKiKo <karolina.kidon@gmail.com>
2018-02-17 18:12:26 +0100

    testListBooksInHandsOf Module6.6

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -1,13 +1,14 @@
 package com.kodilla.testing.weather.mock;
 
-        import com.kodilla.testing.weather.stub.Temperatures;
-        import com.kodilla.testing.weather.stub.WeatherForecast;
-        import org.junit.Assert;
-        import org.junit.Test;
-        import java.util.HashMap;
+import com.kodilla.testing.weather.stub.Temperatures;
+import com.kodilla.testing.weather.stub.WeatherForecast;
+import org.junit.Assert;
+import org.junit.Test;
 
-        import static org.mockito.Mockito.mock;
-        import static org.mockito.Mockito.when;
+import java.util.HashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class WeatherForecastTestSuite {
     @Test
